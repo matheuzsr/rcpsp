74ec7c71170a00a53d495c8e2a81cd3086df3cdd
matheuzsr <matheusdasilvarocha@gmail.com>
2023-05-29 00:05:19 -0300

    Criando coletador de métricas com exec

--- metrics-app/src/index.js
+++ metrics-app/src/index.js
@@ -1,36 +1,55 @@
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
 
-async function lerArquivo(caminhoArquivo) {
+async function lerArquivoInstancia(caminhoArquivo) {
   try {
-    const conteudo = await readFile(caminhoArquivo, 'utf8');
+    const conteudo = await readFile(caminhoArquivo, "utf8");
     return conteudo;
   } catch (erro) {
-    console.error('Erro ao ler o arquivo:', erro);
+    console.error("Erro ao ler o arquivo:", erro);
   }
 }
 
-// alfa_0.000000  alfa_0.200000  alfa_0.500000  alfa_0.800000  alfa_1.000000
+const execs = ["exec_1", "exec_2", "exec_3", "exec_4", "exec_5"];
+const alfas = [
+  "alfa_0.000000",
+  "alfa_0.200000",
+  "alfa_0.500000",
+  "alfa_0.800000",
+  "alfa_1.000000",
+];
+const instancias = [
+  "j301_1",
+  "j301_2",
+  "j609_3",
+  "j609_8",
+  "j1206_4",
+  "j1207_2",
+];
 
-// "j301_1",
-//       "j301_2",
-//       "j609_3",
-//       "j609_8",
-//       "j1206_4",
-//       "j1207_2",
-
-
-const main = async () => {
-
-  const arq = await lerArquivo('../metricas/alfa_1.000000/j1207_2.metric');
-  
+const getMelhorFO = (file) => {
   const regex = /FO:\s*(\d+)/g;
 
-  const matches = arq.match(regex);
-  const FOs = matches.map(match => match.match(/\d+/)[0]);
+  const matches = file.match(regex);
+  const FOs = matches.map((match) => match.match(/\d+/)[0]);
 
-  melhoresFOs = FOs.sort()
+  melhoresFOs = FOs.sort();
+  return melhoresFOs[0];
+};
 
-  console.log(numeros)
-}
+const main = async () => {
+  for (instancia of instancias) {
+    console.log("\nInstancia " + instancia + ":");
+    for (alfa of alfas) {
+      console.log("\nAlfa " + alfa + ":\n");
+      for (execucao of execs) {
+        console.log;
+        const file = await lerArquivoInstancia(
+          `../metricas/${execucao}/${alfa}/${instancia}.metric`
+        );
+        console.log(execucao + ": " + getMelhorFO(file));
+      }
+    }
+  }
+};
 
-main()
+main();
