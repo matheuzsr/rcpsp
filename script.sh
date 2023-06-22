1f303969faac58b42be0b5f2f7b3452110349538
matheuzsr <matheusdasilvarocha@gmail.com>
2023-06-22 19:44:47 -0300

    Incluindo alteração no script

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ make clean && make
 start_time=$(date +%s)
 echo "Hora de início: $(date +%H:%M:%S)"
 
-# ./rcpsp $1
+./rcpsp $1
 
 # Hora de fim
 end_time=$(date +%s)
@@ -16,6 +16,6 @@ echo "Hora de fim: $(date +%H:%M:%S)"
 duration=$((end_time - start_time))
 echo "Tempo gasto: $duration s"
 
-git add "."
+git add "metricas/exec_$1"
 git commit -m "Incluindo execucao $1 - $start_time"
 git push origin
