fa4cd46bf600ab1e4a3c4cd6a6a35150e837e497
matheuzsr <matheusdasilvarocha@gmail.com>
2023-06-22 19:44:05 -0300

    Incluindo execucao 4 - 1687473845

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ make clean && make
 start_time=$(date +%s)
 echo "Hora de início: $(date +%H:%M:%S)"
 
-./rcpsp $1
+# ./rcpsp $1
 
 # Hora de fim
 end_time=$(date +%s)
@@ -15,3 +15,7 @@ echo "Hora de fim: $(date +%H:%M:%S)"
 # Cálculo do tempo gasto
 duration=$((end_time - start_time))
 echo "Tempo gasto: $duration s"
+
+git add "."
+git commit -m "Incluindo execucao $1 - $start_time"
+git push origin
